927e9df6107d65ab136afdfa529350ef44be8f57
unsigned_nerd <unsigned_nerd@member.fsf.org>
2018-04-13 23:41:52 +0700

    This is the first version of function vimtable_to_line_list which passes the test.  The code is really ugly.  I will refactor it later.

--- src/vtmtc.py
+++ src/vtmtc.py
@@ -5,9 +5,9 @@ import sys
 
 def vimtable_to_line_list(vimtable):
 
-    """
-    # regexp patterns
+    # common regexp patterns
     line_sep_pattern = re.compile("^[ ]*\|[\-\+]+\|[ ]*$")
+    empty_str_pattern = re.compile("^[ ]*$")
 
     line = None # each line of vimtable
 
@@ -15,10 +15,36 @@ def vimtable_to_line_list(vimtable):
     for line in vimtable:
         if line_sep_pattern.match(line):
             break
-    """
 
-    yield ['Lorem ipsum dolor sit amet', 'Suspendisse diam. Etiam']
-    yield ['Donec et metus lobortis', 'Quisque nulla, a']
+    # calculate number of columns
+    no_of_cols = line.count('+') + 1
+
+    # walk through the table
+    for line in vimtable:
+        # each line inside vim table starts with |
+        line_inside_vimtable_pattern = re.compile("^[ ]*\|.*$")
+        if not line_inside_vimtable_pattern.match(line):
+            # end of table
+            break
+
+        a_row_list = ['' for i in range(no_of_cols)]
+        separated_lines = line.split('|')
+        for col in range(no_of_cols):
+            a_row_list[col] += separated_lines[col+1].lstrip().rstrip()
+            print('a_row_list[' + str(col) + ']=' + a_row_list[col])
+
+        # walk through a row
+        for line in vimtable:
+            if line_sep_pattern.match(line):
+                # end of row
+                break
+            separated_lines = line.split('|')
+            for col in range(no_of_cols):
+                if not empty_str_pattern.match(separated_lines[col+1]):
+                    a_row_list[col] += \
+                      ' ' + separated_lines[col+1].lstrip().rstrip()
+                print('a_row_list[' + str(col) + ']=' + a_row_list[col])
+        yield a_row_list
 
 def _run(argv):
     """
